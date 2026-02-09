1c704b0dffc24fa8098d03a51463957509b0260f
Erick Vessoli <143609058+er1ck-xyz@users.noreply.github.com>
2026-02-09 15:22:55 -0300

    Home modified

--- meu-projeto/src/pages/Home.jsx
+++ meu-projeto/src/pages/Home.jsx
@@ -1,34 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 export default function Home() {
-  const [dados, setDados] = useState([]);
-
-  useEffect(() => {
-    async function carregar() {
-      const response = await fetch("https://fakestoreapi.com/products");
-      const data = await response.json();
-      setDados(data);
-    }
-    carregar();
-  }, []);
-
-  async function deletarProduto(id) {
-    await fetch(`https://fakestoreapi.com/products/${id}`, {
-      method: "DELETE",
-    });
-
-    // remove do estado local
-    setDados((prev) => prev.filter((produto) => produto.id !== id));
-  }
-
   return (
     <div>
-      {dados.map((produto) => (
-        <div key={produto.id}>
-          <img src={produto.image} alt={produto.title} width="100" />
-          <button onClick={() => deletarProduto(produto.id)}>Deletar</button>
-        </div>
-      ))}
+      <h1>Pagina home</h1>
     </div>
   );
 }
